3a87fdba4793758263734e9abc00ecaf3170401f
David Gong <bearhearted@gmail.com>
2013-08-05 23:01:47 +0800

    add user

--- src/com/ligelong/hibernate/service/UserService.java
+++ src/com/ligelong/hibernate/service/UserService.java
@@ -3,6 +3,8 @@
  */
 package com.ligelong.hibernate.service;
 
+import java.sql.Timestamp;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ligelong.hibernate.core.BaseDao;
 import com.ligelong.hibernate.core.BaseService;
 import com.ligelong.hibernate.entity.UserEntity;
+import com.ligelong.util.Status;
 
 /**
  * <code>UserService</code>
@@ -29,8 +32,23 @@ public class UserService extends BaseService<UserEntity> {
         this.baseDao = baseDao;
     }
     
-    public void addUser() {
+    public void addUser(String username, String password, String email, String phone) {
     	UserEntity user = new UserEntity();
-    	
+    	user.setCreatetime(new Timestamp(System.currentTimeMillis()));
+    	user.setEmail(email);
+    	user.setPassword(password);
+    	user.setPhone(phone);
+    	user.setStatus(Status.ON.getValue());
+    	user.setUsername(username);
+    	this.save(user);
     }
+
+	public UserEntity getUserByPwd(String username, String pwd) {
+		String hql = "from UserEntity where username=? and password=?";
+		if(this.getBaseDao().findCountByHql(hql, new Object[]{username, pwd})<1) {
+			return null;
+		}
+		UserEntity user = this.getBaseDao().findListByHql(hql, new Object[]{username, pwd}).get(0);
+		return user;
+	}
 }
--- src/com/ligelong/product/controller/UserController.java
+++ src/com/ligelong/product/controller/UserController.java
@@ -9,14 +9,18 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ligelong.hibernate.entity.PostEntity;
+import com.ligelong.hibernate.entity.UserEntity;
 import com.ligelong.hibernate.service.PostService;
 import com.ligelong.hibernate.service.UserService;
+import com.ligelong.util.Constants;
+import com.ligelong.util.MD5;
 import com.ligelong.util.WebUtil;
 
 
@@ -33,10 +37,35 @@ public class UserController {
 	public ModelAndView post(HttpServletRequest request,
 			HttpServletResponse response) {
     	Map<String, Object> model = new HashMap<String, Object>();
-    	Integer id = WebUtil.getParameterInteger(request, "id", 0);
-    	
+    	String username = WebUtil.getParameter(request, "username", "");
+    	String pwd = WebUtil.getParameter(request, "pwd", "");
+    	UserEntity user = userService.getUserByPwd(username, MD5.change(pwd)); 
+    	if(user!=null) {
+    		HttpSession session = request.getSession(true);
+			session.setAttribute("user_entity", user);
+			request.setAttribute("user", user);
+			WebUtil.setCookies(response, Constants.USER_ID_COOKIE_NAME, user.getId() + "", 30 * 24 * 60 * 60);
+			model.put("url", "/");
+			return new ModelAndView("user/login_success", model);
+    	}
+   		model.put("info", "登录失败，请重试");    		
     	return new ModelAndView("user/login", model);
     }
+    
+    @RequestMapping(value="/reg.do")
+   	public ModelAndView reg(HttpServletRequest request,
+   			HttpServletResponse response) {
+    	if("on".equals(WebUtil.getParameter(request, "action", ""))) {
+    		String username = WebUtil.getParameter(request, "username", "");
+    		String pwd = WebUtil.getParameter(request, "pwd", "");
+    		String email = WebUtil.getParameter(request, "email", "");
+    		String phone = WebUtil.getParameter(request, "phone", "");
+    		userService.addUser(username, MD5.change(pwd), email, phone);
+    		return new ModelAndView("/user/login", "info", "创建成功，请登录");
+    	}
+    	return new ModelAndView("/user/reg");
+    }
+    
     
     @Resource
     private UserService userService;
